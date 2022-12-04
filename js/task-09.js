5673734c341e771145efe6ebe7c62efbb31199ac
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-04 17:07:26 +0100

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const colorBody = document.querySelector("body")
+const btnChangeColor = document.querySelector(".change-color")
+const currentColor = document.querySelector(".color")
+const changeColor = (event) => {
+    const randomColor = getRandomHexColor()
+    colorBody.style.backgroundColor = randomColor
+    currentColor.textContent = randomColor
+}
+
+btnChangeColor.addEventListener("click", changeColor)
