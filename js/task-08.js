a222fe5d47e62a9e92716f7e528c6704f4f02cf0
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-04 16:53:26 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+const eventHandler = (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Please enter email and password!");
+        } 
+        const userData = {email: email.value, password: password.value}
+        console.log(userData)
+        event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", eventHandler);
