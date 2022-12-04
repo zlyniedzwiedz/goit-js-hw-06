7da21c221beb5ac52ac3229a45f5de8d6b36fbec
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-04 17:59:38 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,14 +4,7 @@ function getRandomHexColor() {
         .padStart(6, 0)}`;
 }
 
-// const btnChangeColor = document.querySelector(".change-color");
-// const currentColor = document.querySelector(".color");
-// const changeColor = (event) => {
-//     const randomColor = getRandomHexColor();
-//     colorBody.style.backgroundColor = randomColor;
-//     };
 
-// btnChangeColor.addEventListener("click", changeColor);
 
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
@@ -20,23 +13,27 @@ const input = document.querySelector("input");
 const divBox = document.querySelector("#boxes");
 //Create
 const createBoxes = (amount) => {
-    let FirstboxSize = 30
+    let FirstboxSize = 30;
     for (let i = 0; i < amount; i++) {
         const box = document.createElement("div");
         divBox.append(box);
         box.style.height = FirstboxSize + "px";
         box.style.width = FirstboxSize + "px";
         box.style.backgroundColor = `${getRandomHexColor()}`;
-
         FirstboxSize += 10;
     }
 };
 createBtn.addEventListener("click", () => {
-  createBoxes(input.value)
+    createBoxes(input.value);
 });
+// Seek And Destroy
 
-function destroyBoxes() {
+const destroyBoxes = () => {
   while (divBox.firstChild) {
-    
+    divBox.removeChild(divBox.firstChild);
   }
-}
+};
+
+destroyBtn.addEventListener("click", () => {
+    destroyBoxes()
+});
