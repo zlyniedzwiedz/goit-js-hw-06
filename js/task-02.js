0e2504123f9c24949bbb9c5086da487ae3a9b2cf
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-04 15:02:22 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,6 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const ingredientsList = document.querySelector("#ingredients");
+
+const markup = ingredients.map((ingredients) => `<li class="item">${ingredients}</li>`).join("");
+
+ingredientsList.innerHTML = markup;
